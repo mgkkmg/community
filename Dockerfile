5ae6622bb9330ccb426ee449b03e69357747fdcc
mgkkmg <kmnine3@gmail.com>
2023-06-10 17:23:41 +0900

    aws ec2 배포 자동화 설정

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:17
 ARG JAR_FILE=modual-api/build/libs/app.jar
 COPY ${JAR_FILE} ./app.jar
 ENV TZ=Asia/Seoul
-ENTRYPOINT ["java", "java ${JAVA_OPTS} -jar", "./app.jar"]
+ENTRYPOINT ["java", "-jar", "./app.jar", "-Dspring.profiles.active=prod"]
--- module-core/src/main/java/com/gym/modulecore/config/security/AuthenticationConfig.java
+++ module-core/src/main/java/com/gym/modulecore/config/security/AuthenticationConfig.java
@@ -1,6 +1,7 @@
 package com.gym.modulecore.config.security;
 
 import com.gym.modulecore.config.security.filter.JwtTokenFilter;
+import com.gym.modulecore.core.user.repository.LogoutTokenRepository;
 import com.gym.modulecore.core.user.service.UserService;
 import com.gym.modulecore.exception.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
@@ -9,45 +10,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
 @Configuration
 public class AuthenticationConfig {
 
     private final UserService userService;
-
+    private final LogoutTokenRepository logoutTokenRepository;
     @Value("${jwt.token.access.secret-key}")
     private String accessKey;
 
     @Value("${jwt.token.access.secret-key}")
     private String refreshKey;
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .regexMatchers("^(?!/api/).*") // /api/로 시작하지 않는 모든 문자열을 일치
-                .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login", "/api/*/users/reissue");
-    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring()
+//                .regexMatchers("^(?!/api/).*") // /api/로 시작하지 않는 모든 문자열을 일치
+//                .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login", "/api/*/users/reissue");
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(authorize -> authorize
-//                        .regexMatchers("^(?!/api/).*").permitAll() // /api/로 시작하지 않는 모든 문자열을 일치
-//                        .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login").permitAll()
+                        .regexMatchers("^(?!/api/).*").permitAll() // /api/로 시작하지 않는 모든 문자열을 일치
+                        .antMatchers(HttpMethod.POST, "/api/*/users/join", "/api/*/users/login").permitAll()
                         .antMatchers("/api/**").authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(new JwtTokenFilter(accessKey, userService), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenFilter(accessKey, userService, logoutTokenRepository), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new CustomAuthenticationEntryPoint()) // 인증되지 않은 사용자의 리소스에 대한 접근 처리
                 )
